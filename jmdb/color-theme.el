45a7942ebd335098bcf5d7561d342b15f3533fb4
Jim Barritt <jim@planet-ix.com>
2011-06-12 20:07:51 +0100

    [jmdb] updating color theme for modeline

--- jmdb/color-theme.el
+++ jmdb/color-theme.el
@@ -4,13 +4,24 @@
 (add-to-list 'load-path "~/emacs/color-theme")
 (require 'color-theme)
 (color-theme-initialize)
+
+;;(color-theme-gnome)
 (color-theme-charcoal-black)
 
+;; Jims' Customisations:
 
-(set-background-color "black")
-(set-foreground-color "white")
-(set-face-foreground 'modeline "#777777")
-(set-face-background 'modeline "#222222")
+;; The modeline:
+
+(custom-set-faces
+   '(mode-line ((t (:foreground "#777777" :background "#222222" :box nil))))
+   '(mode-line-inactive ((t (:foreground "#555555" :background "#222222" :box nil)))))
+
+;; Can set them individually like this:
+;; (set-face-foreground 'modeline "#777777")
+
+;; The edge of the screen
+(set-face-background 'fringe "#111111")
+
 
 (set-cursor-color "purple")
 
